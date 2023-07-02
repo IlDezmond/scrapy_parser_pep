6caf09ee9e9fa2c8ed7a727964042c7c0d59ff20
IlDezmond <desmond578@mail.ru>
2023-07-03 01:49:38 +0500

    First iteration

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,30 @@
+import csv
+from pathlib import Path
+import datetime as dt
+
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class PepParsePipeline:
+    def __init__(self):
+        self.statuses = dict()
+        self.summary = 0
+
+    def open_spider(self, spider):
+        pass
+
+    def process_item(self, item, spider):
+        self.statuses[item['status']] = self.statuses.get(item['status'], 0)+1
+        self.summary += 1
+        return item
+
+    def close_spider(self, spider):
+        results_dir = BASE_DIR / 'results'
+        results_dir.mkdir(exist_ok=True)
+        now = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        file_path = results_dir / f'status_summary_{now}.csv'
+        with open(file_path, 'w', encoding='utf-8') as f:
+            writer = csv.writer(f, dialect='unix')
+            writer.writerows(self.statuses.items())
+            writer.writerow(('Total', self.summary))
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,33 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response, **kwargs):
+        tr_tags = response.css('section[id=numerical-index] tbody tr')
+        for tr_tag in tr_tags:
+            td_tags = tr_tag.css('td')
+            pep_number = td_tags[1].css('::text').get()
+            pep_name = td_tags[2].css('::text').get()
+            pep_link = td_tags[1].css('a::attr(href)').get()
+            yield response.follow(
+                pep_link,
+                callback=self.parse_pep,
+                cb_kwargs={
+                    'number': pep_number,
+                    'name': pep_name,
+                }
+            )
+
+    def parse_pep(self, response, **kwargs):
+        pep_status = response.css(
+            'dt:contains("Status:") + dd abbr::text'
+        ).get()
+        kwargs.update({'status': pep_status})
+
+        yield PepParseItem(kwargs)
